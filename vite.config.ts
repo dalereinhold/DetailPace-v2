cce185f30959b47ccecea9bdb5337497142104e1
Kim Erik Reinhold <dalereinhold@outlook.com>
2026-09-20 20:19:34 +0200

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/detailpace/",
+  base: "/detailpace-v2/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
